badc9f8d639b0bba032cd8833d809123347b0a4d
gil-mahalla <gil.mahalla@cs.huji.ac.il>
2021-06-04 01:13:07 +0300

    gil 3rd page

---
+++ app/src/main/java/com/example/hackidc2021/take_a_picture.kt
@@ -0,0 +1,11 @@
+package com.example.hackidc2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class take_a_picture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_apicture)
+    }
+}
