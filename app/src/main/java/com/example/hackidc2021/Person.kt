3e69c995628e99ad53f12b3fa691ee64cdd536d7
hadasnah <72435482+hadasnah@users.noreply.github.com>
2021-06-03 13:45:41 +0300

    Added basic db functionality + username/password fields.

--- app/src/main/java/com/example/hackidc2021/Person.kt
+++ app/src/main/java/com/example/hackidc2021/Person.kt
@@ -2,8 +2,20 @@ package com.example.hackidc2021
 
 class Person
 {
-    init {
+    var iD: Long = 0
+    var name: String? = null
+    var phoneNumber: String? = null
 
+    constructor() {}
+    constructor(id: Long, name: String?, _phone_number: String?) {
+        iD = id
+        this.name = name
+        phoneNumber = _phone_number
+    }
+
+    constructor(name: String?, _phone_number: String?) {
+        this.name = name
+        phoneNumber = _phone_number
     }
 
 }
