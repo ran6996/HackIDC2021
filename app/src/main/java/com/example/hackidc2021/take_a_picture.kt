268d0f6f0b003bfd2bfadda598b9f1a204adf433
hadasnah <72435482+hadasnah@users.noreply.github.com>
2021-06-04 09:13:17 +0300

    Everything works.

--- app/src/main/java/com/example/hackidc2021/take_a_picture.kt
+++ app/src/main/java/com/example/hackidc2021/take_a_picture.kt
@@ -6,11 +6,19 @@ import android.os.Bundle
 import android.view.View
 import com.example.hackidc2021.CameraUsage.TakePicture
 import com.example.hackidc2021.ImageUpload.Upload
+import kotlinx.android.synthetic.main.activity_take_apicture.*
 
 class take_a_picture : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_apicture)
+
+        button2.setOnClickListener{
+            val intent = Intent(this, recorder::class.java)
+//            intent.putExtra(PERSON, personDetails)
+//            intent.putExtra(EVENT, eventDetails)
+            startActivity(intent)
+        }
     }
 
     fun PhotoGetter(v: View?) {
